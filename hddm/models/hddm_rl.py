57a7f7057f02d2566b8400a6e2f506f820837c30
yifeichen123 <45802800+yifeichen123@users.noreply.github.com>
2024-02-01 14:39:23 -0800

    debug, d type

--- hddm/models/hddm_rl.py
+++ hddm/models/hddm_rl.py
@@ -1135,8 +1135,8 @@ def wienerRL_like_uncertainty(x, v0, v1, v2, v_interaction, z0, z1, z2, z_intera
     # qval = 0 # default: simultaneous
 
     # if
-    print(type(alpha_pos),type(alpha_neu),type(alpha_neg))
-    print(type(bandit_type))
+    print(alpha_pos.dtype,alpha_neu.dtype,alpha_neg.dtype)
+    print(bandit_type.dtype)
     return wiener_like_rlddm_uncertainty(
     # return wiener_like_rlddm_2step_reg_sliding_window(
         x["rt1"].values,
